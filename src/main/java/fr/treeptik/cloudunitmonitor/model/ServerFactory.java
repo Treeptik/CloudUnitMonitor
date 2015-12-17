a7e8e0caf415fcd7d5020ccea5064914d62a6f6e
Guillaume <g.martial@treeptik.fr>
2015-12-17 15:46:29 +0100

    FIX-add fat jar impl

--- src/main/java/fr/treeptik/cloudunitmonitor/model/ServerFactory.java
+++ src/main/java/fr/treeptik/cloudunitmonitor/model/ServerFactory.java
@@ -1,54 +1,53 @@
 package fr.treeptik.cloudunitmonitor.model;
 
-import fr.treeptik.cloudunitonitor.model.action.JBossAction;
-import fr.treeptik.cloudunitonitor.model.action.JBossAction5;
-import fr.treeptik.cloudunitonitor.model.action.ServerAction;
-import fr.treeptik.cloudunitonitor.model.action.TomcatAction;
+import fr.treeptik.cloudunitonitor.model.action.*;
 
 public class ServerFactory {
 
-	/**
-	 * Return new module with ModuleAction initialized
-	 * 
-	 * @param moduleName
-	 * @return
-	 */
-	public static Server getServer(String imageName) {
+    /**
+     * Return new module with ModuleAction initialized
+     *
+     * @param
+     * @return
+     */
+    public static Server getServer(String imageName) {
 
-		Server server = new Server();
+        Server server = new Server();
 
-		server.setServerAction(getServerAction(imageName, server));
+        server.setServerAction(getServerAction(imageName, server));
 
-		return server;
-	}
+        return server;
+    }
 
-	/**
-	 * Update module with ModuleAction initialized
-	 * 
-	 * @param moduleName
-	 * @return
-	 */
-	public static Server updateServer(Server server) {
-		server.setServerAction(getServerAction(server.getImage().getName(),
-				server));
-		return server;
-	}
+    /**
+     * Update module with ModuleAction initialized
+     *
+     * @param
+     * @return
+     */
+    public static Server updateServer(Server server) {
+        server.setServerAction(getServerAction(server.getImage().getName(),
+                server));
+        return server;
+    }
 
-	private static ServerAction getServerAction(String imageName, Server server) {
+    private static ServerAction getServerAction(String imageName, Server server) {
 
-		ServerAction result = null;
+        ServerAction result = null;
 
-		// TODO : HACK TO REMOVE MODULE VERSION
-		if (imageName.toLowerCase().contains("tomcat")) {
-			result = new TomcatAction(server);
-		} else if (imageName.toLowerCase().contains("jboss-5")) {
-			result = new JBossAction5(server);
-		} else if (imageName.toLowerCase().contains("jboss")) {
-			result = new JBossAction(server);
-		}
+        // TODO : HACK TO REMOVE MODULE VERSION
+        if (imageName.toLowerCase().contains("tomcat")) {
+            result = new TomcatAction(server);
+        } else if (imageName.toLowerCase().contains("jboss-5")) {
+            result = new JBossAction5(server);
+        } else if (imageName.toLowerCase().contains("jboss")) {
+            result = new JBossAction(server);
+        } else if (imageName.toLowerCase().contains("fatjar")) {
+            result = new FatJarAction(server);
+        }
 
-		return result;
+        return result;
 
-	}
+    }
 
 }
